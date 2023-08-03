0a98f4425e281680e99ea4c137dc05d093a35537
Fabricio Vianna Arnecke <fbrcvnn@gmail.com>
2023-08-02 22:38:00 -0300

    adicionado aula 1 com tech helper

---
+++ bloco-intermediario/react-js/aula1-tech-helper/my-app/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import RoutesApp from './config/routes/RoutesApp'
+
+
+function App() {
+
+  return (
+   <RoutesApp />
+  )
+};
+
+export default App
---
+++ bloco-intermediario/react-js/aula1-tech-helper/my-app/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import SectionImg from "../components/SectionImg";
+
+function Home() {
+  return (
+    <>
+      <SectionImg title="Modificado"/>
+
+      <SectionImg title="Contato" />
+    </>
+    
+  )
+}
+
+export default Home;
